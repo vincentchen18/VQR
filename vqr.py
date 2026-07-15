7c7068746cf7aa67d638dcf6a5feddb4ccce2206
vincentchen18 <vincent.y.chen2011@gmail.com>
2026-07-15 12:45:39 +1000

    progress + add make/read flags + add some fail conditions

--- vqr.py
+++ vqr.py
@@ -1,4 +1,4 @@
-import argparse, qrcode, cv2
+import argparse, qrcode, cv2, sys
 from PIL import Image
 # initialisation
 # flags:
@@ -7,11 +7,25 @@ from PIL import Image
 # -o or --output : specify the name/path of the output file i will make this default to qr.png
 # -s or --size : specify a size (optional) minimum 100 for proper scanning
 #
+def make(): #make the qr code
+    pass
+
+def read(): # read a qr code
+
 def main():
     parser = argparse.ArgumentParser(prog="vqr")
     parser.add_argument("-l", "--link")
     parser.add_argument("-e", "--embed")
     parser.add_argument("-o", "--output")
     parser.add_argument("-s", "--size")
+    parser.add_argument("-m", "--make", action="store_true")
+    parser.add_argument("-r", "--read") #accept path
     args = parser.parse_args()
-    print(args)
+    if args.make and args.read is not None:
+        print("Please choose either --make or --read.")
+        sys.exit()
+    if args.read is not None and args.make is None:
+        if args.link is not None or args.embed is not None or args.output is not None or args.size is not None:
+            print("Reading a QR Code does not accept the following fields: link, embed, output, size.")
+            sys.exit()
+
