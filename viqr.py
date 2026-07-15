b3abffdd7dadc0d7bdcac6a44deeffc6f58caa9d
vincentchen18 <vincent.y.chen2011@gmail.com>
2026-07-15 12:56:13 +1000

    add file fail conditions + rebranding

--- viqr.py
+++ viqr.py
@@ -1,4 +1,4 @@
-import argparse, qrcode, cv2, sys
+import argparse, qrcode, cv2, sys, os
 from PIL import Image
 # initialisation
 # flags:
@@ -11,10 +11,12 @@ def make(): #make the qr code
     pass
 
 def read(): # read a qr code
+    pass
 
 def main():
-    parser = argparse.ArgumentParser(prog="vqr")
+    parser = argparse.ArgumentParser(prog="viqr")
     parser.add_argument("-l", "--link")
+    parser.add_argument("-f", "--file") #file path if for some reason their link/data is in a file
     parser.add_argument("-e", "--embed")
     parser.add_argument("-o", "--output")
     parser.add_argument("-s", "--size")
@@ -24,8 +26,19 @@ def main():
     if args.make and args.read is not None:
         print("Please choose either --make or --read.")
         sys.exit()
-    if args.read is not None and args.make is None:
+    if args.read is not None and args.make is None: # reading mode
         if args.link is not None or args.embed is not None or args.output is not None or args.size is not None:
             print("Reading a QR Code does not accept the following fields: link, embed, output, size.")
+            sys.exit()
+        if os.path.isfile(args.file):
+            try:
+                with Image.open(args.file) as img:
+                    img.verify()  # checks the file is a valid, decodable image
+                pass # valid image, move to next step
+            except Exception:
+                print("Your file is either truncated or is not an image file. Please check the magic bytes and make sure the image is correct.")
+                sys.exit()
+        else:
+            print("File path does not exist or target path is not a file.")
             sys.exit()
 
