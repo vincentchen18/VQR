62e140c338552242bf72d2911c8d857d4a6eb52a
vincentchen18 <vincent.y.chen2011@gmail.com>
2026-07-15 15:57:33 +1000

    add some failure conditions for make qr code branch

--- viqr.py
+++ viqr.py
@@ -46,7 +46,7 @@ def main():
             try:
                 with Image.open(args.read) as img:
                     img.verify()  # checks the file is a valid, decodable image
-                pass # valid image, move to next step
+                pass # the image "passed" LMAO
             except Exception:
                 print("Your file is either truncated or is not an image file. Please check the magic bytes and make sure the image is correct.")
                 sys.exit()
@@ -67,5 +67,45 @@ def main():
                 print(f"Multiple links successfully decoded in {args.read}:")
                 for number, link in enumerate(response):
                     print(f"{number+1}. {link}")
+    elif args.read is None and args.make: # make a qr code
+        if args.link is None and args.file is None:
+            print("You are missing data to generate a QR Code from.")
+            sys.exit()
+        if args.link is not None and args.file is not None:
+            print("Please specify only one type of data to generate a QR Code from.")
+            sys.exit()
+        if args.size is not None:
+            try:
+                size = int(args.size)
+                if size < 100:
+                    raise ValueError("too small!")
+            except Exception:
+                print("Please specify a valid integer size in pixels larger than 100.")
+                sys.exit()
+        if args.link is None and args.file is not None: # submit a file but it fails
+            if not os.path.isfile(args.file):
+                print(f"The file {args.file} does not exist.")
+                sys.exit()
+            else:
+                f = open(args.file, "r")
+                lines = list(f.read().strip().split("\n"))
+                if len(lines) > 1:
+                    print(f"The file {args.file} seems to be multiline. Please select a file with one line only, or use -l to directly use a link.")
+                    sys.exit()
+                else:
+                    enc = lines[0]
+        elif args.link is not None and args.file is None: #submit a link
+            enc = args.link
+        if args.embed is not None:
+            if not os.path.isfile(args.embed):
+                print(f"The file {args.embed} does not exist.")
+                sys.exit()
+            try:
+                with Image.open(args.embed) as img:
+                    img.verify()
+            except Exception:
+                print("Your file does not seem to be a valid image.")
+                sys.exit()
+
 
 
